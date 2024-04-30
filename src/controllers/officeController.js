2e47e9d677eedaf64c4d42b54b898d36e417fa5d
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-04-30 22:40:44 +0300

    Refactor listOfficesBycity function to listOffices and improve search functionality

--- src/controllers/officeController.js
+++ src/controllers/officeController.js
@@ -20,21 +20,30 @@ const addOffice = async (req, res) => {
     })
 };
 
-const listOfficesBycity = async (req, res) => {
+const listOffices = async (req, res) => {
 
-    const cityName = req.params.cityname;
-    client.execute(query.getCityId, [cityName], (err, result) => {
-        if (err) console.log(err);
+    // const cityName = req.params.cityname;
+    client.execute(query.getOffices, async (err, result) => {
+        if (err) {
+            res.status(500).json({ success: false, message: "server error" })
+            console.log(err);
+        }
+
         else {
-            const cityId = result.rows[0].city_id;
-            console.log(typeof cityId);
-            console.log(cityId);
-            client.execute(query.listOfficeByCity, [cityId], (err, result) => {
-                if (err) console.log(err);
-                else {
-                    res.status(200).json({ data: result.rows })
-                }
+            let cities = await new Promise((resolve, reject) => {
+                client.execute(query.getCityIds, (err, result) => {
+                    if (err) { reject(err); }
+                    else { resolve(result); }
+                });
+            });
+
+            var response = result.rows.map(item => {
+                let searchValue = cities.rows.find(city => city.city_id.toString() === item.city_id.toString())?.city_name;
+
+                return { key: item.office_id, value: item.office_name, searchValue: searchValue ? `${searchValue} ${item.office_name}` : 'kayıt yok' };
             })
+
+            res.status(200).json({ data: response });
         }
     })
 
@@ -42,5 +51,5 @@ const listOfficesBycity = async (req, res) => {
 
 module.exports = {
     addOffice,
-    listOfficesBycity
+    listOffices
 }
