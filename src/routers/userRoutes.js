4c74f3831e3167b8fc8741204354498d42a9eab6
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-05-02 22:43:01 +0300

    refactor: Update userRoutes to use getUserById instead of getUsers

--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -3,7 +3,7 @@ const userController = require('../controllers/userController');
 const { registerValidation, loginValidation, updateValidation, verifyValidation } = require('../middlewares/validation/userValidation')
 const authorization = require('../middlewares/authentication/auth');
 
-router.get('/get*er', authorization.tokenCheck, userController.getUsers);
+router.get('/getuser', authorization.tokenCheck, userController.getUserById);
 router.post('/register', registerValidation, userController.register);
 router.post('/login', loginValidation, userController.login);
 router.put('/update', authorization.tokenCheck, updateValidation, userController.update);
