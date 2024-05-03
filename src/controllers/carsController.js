5613e347c87e20bde9f20c4c3d6e19c26f5ba9d7
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-05-03 23:45:06 +0300

    feat: Add listCarsByOffice and addCar functions to carsController

--- src/controllers/carsController.js
+++ src/controllers/carsController.js
@@ -1,16 +1,18 @@
-
 const client = require('../db/connection');
-const qeury = require('../db/queries');
+const query = require('../db/queries');
 
 
 
 const listCarsByOffice = (req, res) => {
     const officeName = req.params.officename;
 
-    client.execute(qeury.getOfficeId, [officeName], (err, result) => {
-        if (err) console.log(err);
-        else {
-            const officeId = result.rows[0].office_id;
+    client.execute(query.getOfficeId, [officeName], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ success: false, message: "query error" })
+
+        } else {
+            const officeId = result?.rows[0]?.office_id;
 
             if (!officeId) {
                 console.log('böyle bir ofis yok');
@@ -18,7 +20,7 @@ const listCarsByOffice = (req, res) => {
             }
             else {
                 console.log(officeId)
-                client.execute(qeury.listCars, [officeId], (err, result) => {
+                client.execute(query.listCars, [officeId], (err, result) => {
                     if (err) console.log(err);
                     else {
                         if (!result.rowLength) {
@@ -36,6 +38,23 @@ const listCarsByOffice = (req, res) => {
 
 }
 
+
+const addCar = (req, res) => {
+    const { brand, carImage, fuelType, gearType, rentalStatus, officeId, rentalPrice } = req.body;
+
+    client.execute(query.addCar, [brand, carImage, fuelType, gearType, rentalStatus, officeId, rentalPrice], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(400).json({ message: 'araba eklenirken hata oluştu' });
+        }
+        else {
+            res.status(200).json({ message: 'araba eklendi', data: result })
+        }
+    })
+}
+
+
 module.exports = {
-    listCarsByOffice
+    listCarsByOffice,
+    addCar
 }
