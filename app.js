c02c9a4d84bfa39f555aa22932726dff830ceab7
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-05-03 23:43:03 +0300

    refactor: Add carsRoutes.js for handling car-related routes

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const port = process.env.PORT || 3000;
 const userRoutes = require('./src/routers/userRoutes');
 const cityRoutes = require('./src/routers/cityRoutes');
 const rentalRoutes = require('./src/routers/rentalRoutes');
+const carsRoutes = require('./src/routers/carsRoutes');
+
+
 const app = express();
 app.use(express.json());
 
@@ -18,7 +21,7 @@ app.get('/', async (req, res) => {
 app.use('/api/user', userRoutes);
 app.use('/api/city', cityRoutes);
 app.use('/api/rental', rentalRoutes);
-
+app.use('/api/cars', carsRoutes);
 
 
 
