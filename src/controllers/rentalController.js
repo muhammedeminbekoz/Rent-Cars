1b674dc1679742e2f4c304456d05b6fde07d5088
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-05-07 23:28:00 +0300

    refactor :  checked token exists for renting car

--- src/controllers/rentalController.js
+++ src/controllers/rentalController.js
@@ -2,7 +2,9 @@ const client = require('../db/connection');
 const query = require('../db/queries');
 const jwt = require('jsonwebtoken');
 const moment = require('moment');
+const { isNullOrUndefinedOrEmpty } = require('../utils/helpers/valueHelper');
 moment().format();
+const cron = require('cron')
 
 
 const getRentalsInfo = (req, res) => {
@@ -11,9 +13,8 @@ const getRentalsInfo = (req, res) => {
             console.log(err, 'query error');
         }
         else {
-            res.status(400).json({ success: true, data: result.rows[0] });
-            console.log(result.rows[0]);
-            console.log('işlem başarılı');
+            res.status(400).json({ success: true, dropoffDate: result?.rows });
+
         }
     })
 }
@@ -21,33 +22,40 @@ const getRentalsInfo = (req, res) => {
 
 
 const addResultOfRental = (req, res) => {
-    const { carId, firstname, lastname, startingDate, endingDate, licanceNo, tckNo, dob, dropoffDate, dropoffOfficeId, phoneNumber, pickupDate, pickupOfficeId, }
-        = req.body;
+    try {
+        const { carId, firstname, lastname, startingDate, endingDate, licanceNo, tckNo, dob, dropoffDate, dropoffOfficeId, phoneNumber, pickupDate, pickupOfficeId, }
+            = req.body;
+        const token = req?.headers?.authorization.split(' ')[1];
 
-    const token = req.headers.authorization.split(' ')[1];
-    const verifyedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    const userId = verifyedToken.sub.userId;
-    console.log('userId :', userId);
+        if (isNullOrUndefinedOrEmpty(token)) res.status(403).json({ success: false, message: "Only logged in users can rent" })
+        else {
+            const verifyedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-
-    client.execute(query.addResultOfRental,
-        [carId, firstname, lastname, startingDate, endingDate, licanceNo, tckNo, dob, dropoffDate, dropoffOfficeId, phoneNumber, pickupDate, pickupOfficeId, userId],
-        { prepare: true },
-        (err, result) => {
-            if (err) {
-                console.log(err)
-                res.status(500).json({ success: false, message: "server error" });
-                return result.rows[0];
+            if (!verifyedToken) {
+                res.status(401).json({ success: false, message: "Only logged in users can rent" })
             }
+            const userId = verifyedToken.sub.userId;
 
-            client.execute(query.changeCarInfos, [false, dropoffOfficeId, carId], { prepare: true }, (err, result) => {
-                if (err) res.status(500).json({ success: false, message: "server error" });
-            })
+            client.execute(query.addResultOfRental,
+                [carId, firstname, lastname, startingDate, endingDate, licanceNo, tckNo, dob, dropoffDate, dropoffOfficeId, phoneNumber, pickupDate, pickupOfficeId, userId],
+                { prepare: true },
+                (err, result) => {
+                    if (err) res.status(500).json({ success: false, message: "server error" })
 
-            res.status(200).json({ success: true })
+                    client.execute(query.changeCarInfos, [false, dropoffOfficeId, dropoffDate, carId], { prepare: true }, (err, result) => {
+                        if (err) res.status(500).json({ success: false, message: "server error" });
+                    })
 
+                    res.status(200).json({ success: true, message: "rental saved" })
+
+                }
+            )
         }
-    )
+
+    }
+    catch (err) {
+        res.status(500).json({ success: false, message: "Token expired error please log in again !!!" })
+    }
 
 }
 
