2398a987f23c15b0a58c2337ff4686ba1310b09b
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-05-08 02:23:50 +0300

    feat: Implement automatic car status change based on drop-off date

--- src/controllers/carsController.js
+++ src/controllers/carsController.js
@@ -1,5 +1,6 @@
 const client = require('../db/connection');
 const query = require('../db/queries');
+const nodecron = require('node-cron')
 
 
 
@@ -42,19 +43,72 @@ const listCarsByOffice = (req, res) => {
 const addCar = (req, res) => {
     const { brand, carImage, fuelType, gearType, rentalStatus, officeId, rentalPrice } = req.body;
 
-    client.execute(query.addCar, [brand, carImage, fuelType, gearType, rentalStatus, officeId, rentalPrice], (err, result) => {
+    client.execute(query.addCar, [brand, carImage, fuelType, gearType, rentalStatus, officeId, rentalPrice], { prepare: true }, (err, result) => {
         if (err) {
             console.log(err);
-            res.status(400).json({ message: 'araba eklenirken hata oluştu' });
+            res.status(400).json({ success: false, message: 'server eror' });
         }
         else {
-            res.status(200).json({ message: 'araba eklendi', data: result })
+            res.status(200).json({ success: true, message: 'araba eklendi', })
         }
     })
 }
 
 
+const changeCarStatus = (req, res) => {
+    try {
+
+        client.execute(query.getRentedCars, (err, result) => {
+            if (err) res.status(400).json({ success: false, message: "server error" })
+            else if (result?.rowLength == 0) {
+                res.status(404).json({ success: true, message: "şu anda kiralanmış araç bulunmamaktadır" })
+            }
+            else {
+                const rentedCarsDate = []
+                result.rows.map(value => {
+                    rentedCarsDate.push(value.last_dropoff_date)
+                })
+
+                nodecron.schedule('* * * * * *', () => { // Her 30 dakikada bir çalışacak şekilde ayarlandı,
+                    const today = new Date();
+
+                    rentedCarsDate.forEach((dropoffDate) => {
+                        const carDropoffDate = new Date(dropoffDate);
+                        if (carDropoffDate < today) {
+                            client.execute(query.getCarIdBylastDropoffDate, [dropoffDate], (err, result) => {
+                                if (err) res.status(400).json({ success: false, message: "server error" })
+                                else {
+                                    const carId = result?.rows[0]?.car_id;
+
+                                    client.execute(query.changeRentalStatus, [true, carId], (err, result) => {
+                                        if (err) res.status(400).json({ success: false, message: "server err" })
+                                        else {
+                                            console.log('this car rental_status changed from false to true :', carId);
+                                        }
+                                    })
+
+                                }
+                            })
+                        }
+                    });
+                });
+
+                res.status(200).json({ success: true, data: result.rows })
+
+            }
+        })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(400).json({ success: false, message: "server error" })
+    }
+
+
+}
+
+
 module.exports = {
     listCarsByOffice,
-    addCar
+    addCar,
+    changeCarStatus
 }
